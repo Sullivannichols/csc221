98424b138ee595c0867b58b9a8fa62a7fd7d6f63
Sullivannichols <nicholssullivan@students.belhaven.edu>
2016-04-11 18:45:37 +0000

    changes to hw4 and lab7

--- hw/hw4/hw4.py
+++ hw/hw4/hw4.py
@@ -1,70 +1,88 @@
-#!/usr/bin/env python3
+i#!/usr/bin/env python3
 
-import hw4_solution
+from hw4_solution import (
+    is_odd,
+    is_even,
+    is_mult_of_four,
+    is_mult_of_divisor,
+    both_ends,
+    )
+
 # --------------------------------------------------------------------
 # Problem 1
-# 
+#
 # Create a function, is_odd, that takes a number and returns True if
 # that number is odd.
-# 
+#
 # Function: is_odd
-# 
+#
 # parameters:
 # - number
 #
 # returns: boolean
 
-
+def test_is_odd():
+    assert_is_odd(4)==False
+    assert_is_odd(5)==True
 
 # --------------------------------------------------------------------
 # Problem 2
-# 
+#
 # Create a function, is_even, that takes a number and returns True if
-# that number is even. 
-# 
+# that number is even.
+#
 # Function: is_even
-# 
+#
 # parameters:
 # - number
 #
 # returns: boolean
 
-
+def test_is_even():
+    assert_is-even(4)==True
+    assert_is_even(5)==False
 
 # --------------------------------------------------------------------
 # Problem 3
-# 
+#
 # Create a function, is_mult_of_four, that takes a number and returns
-# True if that number is a multiple of four. 
-# 
+# True if that number is a multiple of four.
+#
 # Function: is_mult_of_four
-# 
+#
 # parameters:
 # - number
 #
 # returns: boolean
+
+def test is_mult_of_four():
+    assert_is_mult_of_four(20)==True
+    assert_is_mult_of_four(21)==False
 
 
 
 # --------------------------------------------------------------------
 # Problem 4
-# 
+#
 # Create a function, is_mult_of_x, that takes a number and a divisor
 # and returns True if that number is divisible by divisor
-# 
+#
 # Function: is_mult_of_divisor
-# 
+#
 # parameters:
 # - number
 # - divisor
 #
 # returns: boolean
 
+def test_is_mult_of_divisor():
+    assert_is_mult_of_divisor(0)==True
+    assert_is_mult_of_divisor(1)==False
 
 
 # --------------------------------------------------------------------
 # Problem 5
-# 
+#
 # Given a string s, return a string made of the first 2 and the last 2
 # chars of the original string, so 'spring' yields 'spng'. However, if
 # the string length is less than 2, return instead the empty string.
@@ -75,3 +93,19 @@ import hw4_solution
 # - s
 #
 # returns: string
+def test_both_ends():
+    imports=[
+        ('spring', 'spong'),
+        ('a', ''),
+        ("",""),
+        ('ab','abab'),
+        ('abc', 'abbc'),
+        ('abcd', 'abcd'),
+
+    for iarg, output in inputs:
+        assert both_ends(arg)==output
+
+
+
+
+
--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -1,4 +1,4 @@
-# --------------------------------------------------------------------
+# --------------------------------------------------------------------:
 # Unit Testing
 #
 # In this lab you will be writing unit tests for the programming
@@ -9,17 +9,20 @@
 # The problem is that each of the solutions has an error in it. Your
 # unit tests should find these errors.
 
+import lab7_solution as sol
+
+from lab_7 solution import count_letters, reverse_string, is palindrome:
 
 
 # --------------------------------------------------------------------
 # Problem 1
-# 
+#
 # Fix ducklings' names
-# 
+#
 # In Robert McCloskey’s book Make Way for Ducklings, the names of the
 # ducklings are Jack, Kack, Lack, Mack, Nack, Ouack, Pack, and
 # Quack. This loop tries to output these names in order.
-# 
+#
 # Of course, that’s not quite right because Ouack and Quack are
 # misspelled. Can you fix it?
 #
@@ -28,7 +31,7 @@
 
 # --------------------------------------------------------------------
 # Problem 2
-# 
+#
 # Letter count
 #
 # Modify the count_letters function below so that:
@@ -38,6 +41,19 @@
 #
 # Essentially, the function is returning the number of occurances of the
 # parameter char in the parameter string.
+from lab7_solution import (
+    count_letters,
+    reverse_string,
+    is_palindrome,
+)
+
+def test_count_letters():
+    inputs = [
+        ('apple','p', 2),
+        ('ate','t', 1),
+        ('bite','x', 0),
+        ('', 'a', 0),
+
 
 
 
@@ -45,18 +61,24 @@
 # Problem 3
 #
 # Reversing a string
-# 
+#
 # Complete the following function such that it reverses the parameter
 # string.
-
-
-
+def reverse_string():
+    inuts = [
+        ('hello', 'olleh')
+]
+    for string correct in inputs:
+     reverse_string(string) == correct
 # --------------------------------------------------------------------
 # Problem 4
-# 
+#
 # Checking for palindromes
-# 
+#
 # Complete the following such that it correctly determines whether the
 # given parameter, string, is a palindrome
-# 
+#
+$string = "A man, a plan, a canal, Panama";
+
+function is_palindrome($string)
 
