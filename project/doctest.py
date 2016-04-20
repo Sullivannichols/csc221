3b0f7d98055719b3918c4faf30641030b2323b4a
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-20 02:05:29 -0500

    Create doctest.py

---
+++ project/doctest.py
@@ -0,0 +1,21 @@
+import doctest
+
+def year(n):
+    """ 
+    Calculates the n-th is a leap year 
+    >>> year(2016)
+    is a leap year
+    >>> year(1994)
+    is not a leap year
+    >>> year(1492) 
+    is a leap year
+    >>> year(1983)
+    is not a leap year
+    >>> 
+
+    """
+
+    return True
+
+if __name__ == "__main__": 
+    doctest.testmod()
