f59966033cafcda47e08dfd87d65d1db22f51e9d
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-25 01:50:33 -0500

    Create RNA_doctest.py

---
+++ project/RNA_doctest.py
@@ -0,0 +1,24 @@
+import doctest
+
+from dna import to_rna
+
+def dna(n):
+    """ 
+     replacing each nucleotide with its complement
+    >>> dna(G)
+    C
+    >>> dna(C)
+    G
+    >>> dna(T) 
+    A
+    >>> dna(A)
+    U
+    >>> 
+    """
+
+    return True
+
+if __name__ == "__main__": 
+    doctest.testmod()
+    test Pass
+    
