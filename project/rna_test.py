f9526047414ff113171451effd780d10bba43f00
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-25 01:01:00 -0500

    Update rna_test.py

--- project/rna_test.py
+++ project/rna_test.py
@@ -17,8 +17,26 @@ class DNATests(unittest.TestCase):
     def test_transcribes_adenine_to_uracil(self):
         self.assertEqual('U', to_rna('A'))
 
-    def test_transcribes_all_occurences(self):
+    def test_transcribes_all(self):
         self.assertEqual('GCTA', to_rna('CGAU'))
+        
+    def test_transcribes_guanine_to_cytosine(self):
+        self.assertEqual('c', to_rna('g'))
+
+    def test_transcribes_cytosine_to_guanine(self):
+        self.assertEqual('g', to_rna('c'))
+
+    def test_transcribes_thymine_to_adenine(self):
+        self.assertEqual('a', to_rna('t'))
+
+    def test_transcribes_adenine_to_uracil(self):
+        self.assertEqual('u', to_rna('a'))
+
+    def test_transcribes_all(self):
+        self.assertEqual('gcta', to_rna('cgau'))
+        
+        
+    
 
 
 if __name__ == '__main__':
