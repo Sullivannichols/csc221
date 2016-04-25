d56c5151e6fcc99bbe06f174966bffc232819c6a
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-25 00:49:28 -0500

    Update RNA Transcription.py

--- project/RNA Transcription.py
+++ project/RNA Transcription.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+
+print("\n")
+
+user = int(input("Enter a dna to check\n:"))
+
+dna[0] 
+'C' 
+dna[1] 
+'T' 
+dna[2] 
+'G' 
+dna[3] 
+'A' 
+
+upper = raw_input('type in something lowercase.')
+lower = raw_input('type in the same thing caps lock.')
+print upper.upper()
+print lower.lower()
+
+
 def transcribe(dna): 
      """Return dna string as rna string.""" 
      return dna.replace('A', 'U'),('G','C'),('T','A')('C','G') 
