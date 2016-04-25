2bce6128e13147323a6721c9cc77a61a940a0b21
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-25 00:21:26 -0500

    Create RNA Transcription.py

---
+++ project/RNA Transcription.py
@@ -0,0 +1,8 @@
+def transcribe(dna): 
+     """Return dna string as rna string.""" 
+     return dna.replace('A', 'U'),('G','C'),('T','A')('C','G') 
+     
+     transcribe('GCTU')
+     'CGAU'
+
+      
