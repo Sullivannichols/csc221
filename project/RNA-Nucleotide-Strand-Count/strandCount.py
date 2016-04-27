049ec62d11c582b16f9616047b22690d8a498a2b
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-27 00:19:26 -0500

    Create strandCount.py

---
+++ project/RNA-Nucleotide-Strand-Count/strandCount.py
@@ -0,0 +1,23 @@
+import unittest
+
+from dna import rna_strand_count
+
+from strands import rna_strand_count
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_rna_strand_count(self):
+      self.assertStrand('AAAA', ['AA'])
+      self.assertStrand('CCCC', ['CC'])
+      self.assertStrand('GGGG', ['GG'])
+      self.assertStrand('TTTT', ['TT'])
+      
+  def test_rna_dna_count(self):
+      self.assertDna('TTT', ['TT'])
+      self.assertDna('GGG', ['GG'])
+      self.assertDna('CCCC', ['CC'])
+      self.assertDna('UUUU', ['UU'])
+
+
+if __name__ == '__main__':
+    unittest.main()
