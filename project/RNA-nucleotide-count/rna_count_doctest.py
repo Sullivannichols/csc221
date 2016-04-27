3b4eada704c317673c4f2bf11bb39681bacdefef
Sullivannichols <nicholssullivan994@yahoo.com>
2016-04-26 22:34:07 -0500

    Create rna_count_doctest.py

---
+++ project/RNA-nucleotide-count/rna_count_doctest.py
@@ -0,0 +1,23 @@
+import doctest
+
+from RNA Nucleotide Count import rna_count
+
+def rna_count(dna):
+    """ 
+    Count the n-th is a rna_count(dna)
+    >>> dna('A')
+    totalA += 1
+    >>> dna('C')
+    totalC += 1
+    >>> dna('G') 
+    totalG += 1
+    >>> dna('T')
+    totalT += 1
+    >>> 
+    """
+
+    return True
+
+if __name__ == "__main__": 
+    doctest.testmod()
+    test Pass
