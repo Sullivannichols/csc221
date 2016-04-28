2f05da08e63931ff785486899e0b91603be7ce5b
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-04-27 19:50:57 -0500

    Create strandCount.py

---
+++ project/RNA-Nucleotide-Strand-Count/strandCount.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def rna_strand_count(test_string,sub_string):
+    test_string = test_string.replace(' ','').upper()    
+    sub_string = sub_string.replace(' ','').upper()    
+    first = sub_string[0]
+    second = sub_string[1:]
+    return {sub_string:len(re.findall(r'{0}(?={1})'.format(first,second),test_string))}
+
+
+print rna_strand_count('AAAA','AA')
