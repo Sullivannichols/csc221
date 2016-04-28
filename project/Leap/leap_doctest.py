023dd3f869f58b71a943bc8185e12bdf93e80178
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-04-27 20:15:58 -0500

    Update leap_doctest.py

--- project/Leap/leap_doctest.py
+++ project/Leap/leap_doctest.py
@@ -4,14 +4,13 @@ from year import is_leap_year
 
 def year(n):
     """ 
-    Calculates the n-th is a leap year 
-    >>> year(2016)
+    >>> is_leap_year(2016)
     is a leap year
-    >>> year(1994)
+    >>> is_leap_year(1994)
     is not a leap year
-    >>> year(1492) 
+    >>> is_leap_year(1492) 
     is a leap year
-    >>> year(1983)
+    >>> is_leap_year(1983)
     is not a leap year
     >>> 
 
