795e967f3c41f14dc46d542aaed8cc285a69dbee
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-05-01 13:59:54 -0500

    Update Leap_test.py

--- project/Leap/Leap_test.py
+++ project/Leap/Leap_test.py
@@ -5,18 +5,11 @@ from year import is_leap_year
 class TestStringMethods(unittest.TestCase):
 
   def test_is_leap_year(self):
-      self.assertTrue(isLeapYear(2016));
-      self.assertFalse(isLeapYear(1994));
+      self.assertIs(is_leap_year(2016), True)
+      self.assertFalse(is_leap_year(1994));
+      self.assertIs(is_leap_year(1492), False)
+      self.assertFalse(is_leap_year(1983));
 
-  def test_is_leap_year(self):
-      self.assertTrue(isLeapYear(1492));
-      self.assertFalse(isLeapYear(1983));
 
-  
 if __name__ == '__main__':
      unittest.main()
-
-
-
-
-
