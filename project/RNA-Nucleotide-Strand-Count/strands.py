abf980151f383692ed9f877e2197422c3c77d980
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-05-01 15:31:18 -0500

    Update strands.py

--- project/RNA-Nucleotide-Strand-Count/strands.py
+++ project/RNA-Nucleotide-Strand-Count/strands.py
@@ -29,10 +29,9 @@ def rna_strand_count(dna, strands):
     ({'AA': 3},{'CC': 2},{'GG': 1},{'TT': 4},{'aa': 3},{'cc': 2},{'gg': 1},{'tt': 4},{'uu': 4})
     """
 
-    dna.upper = dna.replace(' ','').upper()
-    strands.upper = strands.replace(' ','').upper()
+    dna = dna.replace(' ','').upper()
+    strands = strands.replace(' ','').upper()
     first = strands[0]
     second = strands[1:]
     return {strands:len(re.findall(r'{0}(?={1})'.format(first,second),dna))}
-    dna.lower = dna.replace(' ','').lower()
-    strands.lower = strands.replace(' ','').lower()
+    
