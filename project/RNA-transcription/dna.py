486d06b4091b8ae4f2addf4fb977218254d8dc10
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-04-30 23:31:23 -0500

    Update dna.py

--- project/RNA-transcription/dna.py
+++ project/RNA-transcription/dna.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 
-print("\n")
-
-user = int(input("Enter a dna to check\n:"))
-
-upper = raw_input('type in something lowercase.')
-lower = raw_input('type in the same thing caps lock.')
-print upper.upper()
-print lower.lower()
 
 
 def to_rna(dna):
@@ -16,13 +8,12 @@ def to_rna(dna):
 complement = {'G': 'C',
               'C': 'G',
               'T': 'A',
-              'A': 'U'}
+              'A': 'U',
+              'g': 'c',
+              'c': 'g',
+              't': 'a',
+              'a': 'u'}
 
-def transcribe(dna): 
-     """Return dna string as rna string.""" 
-     return dna.replace('A', 'U'),('G','C'),('T','A')('C','G') 
-     
-     print transcribe('GCTA')
-     'CGAU'
+
 
       
