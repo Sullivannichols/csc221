1adb1022070274dadad53b74ff680758ceaea514
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-05-01 14:05:07 -0500

    Update RNA_ Nucleotide_ Count_test.py

--- project/RNA-Nucleotide-Count/RNA_ Nucleotide_ Count_test.py
+++ project/RNA-Nucleotide-Count/RNA_ Nucleotide_ Count_test.py
@@ -2,20 +2,16 @@ import unittest
 
 from dna Nucleotide Count import rna_count
 
-   
-    class RNA NucleotideCountTests(unittest.TestCase):
-    def test_rna_count(self):
+class DNATests(unittest.TestCase):
+   def test_rna_count(self):
         self.assertEqual(
             {'letter': 1},
             rna_count('letter')
         )
-    
+
     def test_count_one_of_each(self):
         self.assertEqual(
-            {'A': 1, 'C': 1, 'G': 1, 'U': 1},
-            letter_count('A C G U')
+            {'A': 1, 'C': 1, 'G': 1, 'U': 1,'a': 1, 'c': 1, 'g': 1, 'u': 1},
+            rna_count('A C G U a c g u')
         )
 
-
-if __name__ == '__main__':
-    unittest.main()
