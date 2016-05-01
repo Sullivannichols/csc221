7fad64ca43f24f39cf3fc77be1202a7538b9d88f
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-05-01 14:03:41 -0500

    Update dna.py

--- project/RNA-Nucleotide-Count/dna.py
+++ project/RNA-Nucleotide-Count/dna.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 def rna_count(dna):
+
+    """
+    >>> rna_count('A')
+    1
+    >>> rna_count('CCC')
+    3
+    >>> rna_count('GGGG')
+    4
+    >>> rna_count('UU')
+    2
+    >>> rna_count('ACCCGGGGUU')
+    rna_count({'A': 1, 'C': 3, 'G': 4, 'U': 2})
+    >>> rna_count('a')
+    1
+    >>> rna_count('ccc')
+    3
+    >>> rna_count('gggg')
+    4
+    >>> rna_count('uu')
+    2
+    >>> rna_count('acccgggguu')
+    rna_count({'a': 1, 'c': 3, 'g': 4, 'u': 2})
+    """
+
+
     d = {}
 # count occurances of character
     for w in dna:
@@ -9,7 +34,7 @@ def rna_count(dna):
     for k in sorted(d):
         print (k + ': ' + str(d[k]))
 
-dna='ACCTTTGGGCCCAAACCCCGGGUUGGAAACCCTTT'
-rna_count(dna)
+
+
 
         
