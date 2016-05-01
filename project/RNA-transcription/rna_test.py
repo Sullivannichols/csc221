dca9884296f3892fece0ee290f9b12f9aa0df724
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-05-01 14:02:57 -0500

    Update rna_test.py

--- project/RNA-transcription/rna_test.py
+++ project/RNA-transcription/rna_test.py
@@ -7,37 +7,22 @@ class DNATests(unittest.TestCase):
 
     def test_transcribes_guanine_to_cytosine(self):
         self.assertEqual('C', to_rna('G'))
+        self.assertEqual('c', to_rna('g'))
+
 
     def test_transcribes_cytosine_to_guanine(self):
         self.assertEqual('G', to_rna('C'))
-
-    def test_transcribes_thymine_to_adenine(self):
-        self.assertEqual('A', to_rna('T'))
-
-    def test_transcribes_adenine_to_uracil(self):
-        self.assertEqual('U', to_rna('A'))
-
-    def test_transcribes_all(self):
-        self.assertEqual('GCTA', to_rna('CGAU'))
-        
-    def test_transcribes_guanine_to_cytosine(self):
-        self.assertEqual('c', to_rna('g'))
-
-    def test_transcribes_cytosine_to_guanine(self):
         self.assertEqual('g', to_rna('c'))
 
     def test_transcribes_thymine_to_adenine(self):
+        self.assertEqual('A', to_rna('T'))
         self.assertEqual('a', to_rna('t'))
 
     def test_transcribes_adenine_to_uracil(self):
+        self.assertEqual('U', to_rna('A'))
         self.assertEqual('u', to_rna('a'))
 
     def test_transcribes_all(self):
+        self.assertEqual('GCTA', to_rna('CGAU'))
         self.assertEqual('gcta', to_rna('cgau'))
-        
-        
-    
 
-
-if __name__ == '__main__':
-    unittest.main()
