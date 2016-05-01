3965a43ed41f552db30a8179f1c6be7e927dc0f6
Nichols Sullivan <nicholssullivan994@yahoo.com>
2016-04-30 23:34:22 -0500

    Update dna.py

--- project/RNA-transcription/dna.py
+++ project/RNA-transcription/dna.py
@@ -4,6 +4,31 @@
 
 def to_rna(dna):
     return ''.join(complement[nucleotide] for nucleotide in dna)
+    
+  """ 
+     replacing each nucleotide with its complement
+    >>> to_rna(G)
+    C
+    >>> to_rna(C)
+    G
+    >>> to_rna(T) 
+    A
+    >>> to_rna(A)
+    U
+    >>> to_rna(g)
+    c
+    >>> to_rna(c)
+    g
+    >>> to_rna(t) 
+    a
+    >>> to_rna(a)
+    u
+    >>> 
+    """
+
+
+    return True
+
 
 complement = {'G': 'C',
               'C': 'G',
@@ -13,7 +38,6 @@ complement = {'G': 'C',
               'c': 'g',
               't': 'a',
               'a': 'u'}
-
-
-
+              
+    
       
